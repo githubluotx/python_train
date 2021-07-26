9170f79c3220b78720c9ee71dbb90ec715b7731d
luotx <1171687441@qq.com>
2021-07-26 23:13:08 +0800

    the chapter 3 remain train

--- book_cxsjsxuff/training3_8.py
+++ book_cxsjsxuff/training3_8.py
@@ -1,10 +1,9 @@
 """think and way of programming design homework training
 """
 import math
-import re
 
 
-def train_2_8_12(radius):
+def train_3_12(radius):
     """calculate the volume and surface area of the sphere
     input the radius type: int
     """
@@ -13,7 +12,7 @@ def train_2_8_12(radius):
     return volume, surface
 
 
-def train_2_8_13():
+def train_3_13():
     """calculate the distance between two point
     input:two point coordinate, use tuple type"""
     x1 = int(input('input x1:'))
@@ -24,7 +23,7 @@ def train_2_8_13():
     print('\n the distance is:%.3f' % dist)
 
 
-def train_3_8_():
+def train_4_8_():
     num = int(input("input the number:"))
     if num > 0:
         print("Positive")
@@ -47,13 +46,51 @@ def train_cal_bmi():
     return 0
 
 
+def train_3_14():
+    high = float(input("input the high(m):"))
+    high = high / 0.305
+    high_foot = int(high)
+    high_inches = (high - high_foot) * 12
+    print("{}foot {:.3f}inches".format(high_foot, high_inches))
+
+
+def train_3_15():
+    """calculate the average of five numbers
+    """
+    num1 = float(input("input the first number:"))
+    num2 = float(input("input the second number:"))
+    num3 = float(input("input the third number:"))
+    num4 = float(input("input the forth number:"))
+    num5 = float(input("input the firth number:"))
+    ave = (num1 + num2 + num3 + num4 + num5) / 5
+    print("the average is :{}".format(ave))
+
+
+def train_3_22():
+    num2letter = {'5': "A", '4': "B", '3': "C", '2': "D", '1': "E", '0': "F"}
+    num = input("input the number:")
+    print("the letter is:{}".format(num2letter[num]))
+
+
+def train_3_23():
+    num = int(input("input the grades(0-100):"))
+    if num >= 90:
+        print("A")
+    elif num >= 80:
+        print("B")
+    elif num >= 70:
+        print("C")
+    elif num >= 60:
+        print("D")
+    else:
+        print("F")
+
+
 # Train_JudgePosOrNag()
 # Train_CalBMI()
 # print(str(train_2_8_12(4)))
 # train_2_8_13()
-
-s = 'ERRC_PHY_tttt'
-if re.match('ERRC_PHY', s):
-    print(True)
-else:
-    print(False)
+# train_3_14()
+# train_3_15()
+# train_3_22()
+train_3_23()
